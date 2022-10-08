0187e118f811a5a60afb567a83e1ec56aa86add7
Tristan Dinh <92371193+TekTristan@users.noreply.github.com>
2022-10-08 01:56:57 -0700

    Add files via upload

---
+++ CSC 15/PrintTennessee.java
@@ -0,0 +1,36 @@
+/*
+1. Tristan Dinh
+2. 2/07/2022
+3. In this lab we are calling multiple functions within one main function to display a big word Tennessee
+4. My grade is an A because I met all the requirements.
+*/
+
+//remove all my comments and provide your own before submitting your code inclduing this line
+public class TennesseeDinh
+{
+  public static void main(String[] args)
+  {
+     //your code , call other methods you wrote to create the output
+  }
+  /*add block commnet here*/
+  public static void printDoubleT() // should display two T side by side
+  {
+     system.out.println("TT");
+  }
+  /*add block commnet here*/
+  public static void printDoubleE()// should display 2 E side by side
+  {
+    system.out.println("EE");
+  }
+  /*add block commnet here*/
+  public static void printDoubleN()//should display 2 N side by side
+  {
+   system.out.println("NN")
+  }
+  /*add block commnet here*/ 
+  public static void printDoubleS()//should display 2 s side by side
+  {
+                
+     
+  }
+}
